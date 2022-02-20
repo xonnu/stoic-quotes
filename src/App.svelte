4a6455d83708a36aaf61688daafc24b71867dc58
Justin Pascual <chrono.social@outlook.com>
2022-02-20 16:49:23 +0800

    ✨ Add SPA feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,36 @@
 <script>
-  import logo from './assets/svelte.png'
-  let counter = 0;
+  import Router from 'svelte-spa-router'
+  import rick from './assets/rick-roll.gif'
+  // Routes
+  import About from './routes/About.svelte';
+  import Contact from './routes/Contact.svelte';
+import Home from './routes/Home.svelte';
+
+  let routes =  {
+    "/" : Home,
+    "/about" : About,
+    "/contact" : Contact
+  }
+
 </script>
 
-<main class="h-screen w-full bg-black flex flex-col items-center justify-center">
-  <img src={logo} class="w-20 mb-10" alt="">
+<main class="main">
+  
+  <img src={rick} class="w-20 mb-10" alt="">
   <h2 class="font-sans text-white text-2xl">Chrono Svelte Template</h2>
-  <button class="mt-4 p-4  rounded-lg bg-white" on:click={()=> {counter++}}>Counter: {counter}</button>
+  <nav>
+    <ul class="flex flex-row gap-4">
+      <li><a href="#/">Home</a></li>
+      <li><a href="#/about">About</a></li>
+      <li><a href="#/contact">Contact</a></li>
+    </ul>
+  </nav>
+
+  <Router {routes}/>
 </main>
 
 <style>
-
+  .main {
+    @apply h-screen w-full bg-gray-400 flex flex-col items-center justify-center;
+  }
 </style>
