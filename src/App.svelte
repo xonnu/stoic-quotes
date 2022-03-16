468e7b36934f34b386f07184fbc6c980a04b5e97
Justin Pascual <chrono.social@outlook.com>
2022-03-16 13:19:53 +0800

    💄 font and design added

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,19 @@
 
 </script>
 
-<main>
-
+<main class="w-full h-screen ">
+  <div class="container mx-auto flex items-center justify-center h-full">
+    <div class="max-w-[1000px] flex flex-col gap-10">
+      <h2 class="font-primary text-7xl ">Hello, World!</h2>
+      <p class="text-left block w-full font-secondary">— Unknown</p>
+    </div>
+  </div>
 </main>
 
+<footer class="absolute bottom-0 w-full">
+  <p class="text-gray-300 font-secondary text-sm p-4 text-center block w-full ">Created by <a class="underline underline-offset-2 decoration-2" href="http://github.com/heychrono" target="_blank" rel="noopener noreferrer">Chrono</a></p>
+</footer>
+
 <style>
-  
+
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx,svelte}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: "'Noto Serif Display', serif",
+        secondary: "'Poppins', sans-serif;"
+      }
+    },
   },
   plugins: [],
 }
